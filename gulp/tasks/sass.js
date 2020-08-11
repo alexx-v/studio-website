b59ac52b6d4c19801ccf7e322864481d01bc26d8
alexx-v <aleksandr1vlk@gmail.com>
2020-08-11 13:20:55 +0300

    docs folder for github pages

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,16 +1,15 @@
 module.exports = function () {
-  $.gulp.task('sass', function () {
-    return $.gulp.src('sass/style.scss')
-        .pipe($.gp.plumber())
-        .pipe($.gp.sourcemaps.init())
-        .pipe($.gp.sass())
-        .pipe($.gp.autoprefixer({
-          browsers: ['last 2 versions']
-        }))
-        .pipe($.gp.csso())
-        .pipe($.gp.rename('style.min.css'))
-        .pipe($.gp.sourcemaps.write(''))
-        .pipe($.gulp.dest('build/css'))
-        .pipe($.browserSync.stream());
-  });
+	$.gulp.task('sass', function () {
+		return $.gulp
+			.src('sass/style.scss')
+			.pipe($.gp.plumber())
+			.pipe($.gp.sourcemaps.init())
+			.pipe($.gp.sass())
+			.pipe($.gp.autoprefixer())
+			.pipe($.gp.csso())
+			.pipe($.gp.rename('style.min.css'))
+			.pipe($.gp.sourcemaps.write(''))
+			.pipe($.gulp.dest('build/css'))
+			.pipe($.browserSync.stream());
+	});
 };
